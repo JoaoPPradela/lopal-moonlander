94881b02dbd2805bdedfe838ad2a71b62d1a2636
JoaoPPradela <joao.pradela@aluno.senai.br>
2025-03-28 16:45:11 -0300

    Segunda versão Preliminar

---
+++ jogo.js
@@ -0,0 +1,140 @@
+//Moonlander. Um jogo de alunissagem.
+// João Pedro dos Santos Pradela
+//https://github.com/JoaoPPradela
+// 28/03/2025
+// Versão 0.1
+/** @type {HTMLCanvasElement} */
+
+//Seção de Modelagem de dados
+let canvas = document.querySelector("#jogo");
+let contexto = canvas.getContext("2d");
+
+let moduloLunar = {
+    posicao: {
+        x: 100,
+        y: 100
+    },
+    angulo: 0,
+    largura: 20,
+    altura: 20,
+    cor: "lightgray",
+    motorLigado: false,
+    velocidade: {
+        x: 0,
+        y: 0
+    },
+    combustivel: 1000
+}
+
+//Seção de visualização
+function desenharModuloLunar(){
+    contexto.save();
+    contexto.beginPath();
+    contexto.translate(moduloLunar.posicao.x, moduloLunar.posicao.y);
+    contexto.rotate(moduloLunar.angulo);
+    contexto.rect(moduloLunar.largura * -0.5, moduloLunar.altura * -0.5,
+         moduloLunar.largura, moduloLunar.altura);
+    contexto.fillStyle = moduloLunar.cor;
+    contexto.fill();
+    contexto.closePath();
+
+    if(moduloLunar.motorLigado){
+        desenharChama();
+        consumoCombustivel();
+    }
+    
+    contexto.restore();
+}
+
+function desenharChama(){
+    contexto.beginPath();
+    contexto.moveTo(moduloLunar.largura * -0.5, moduloLunar.altura * 0.5);
+    contexto.lineTo(moduloLunar.largura * 0.5, moduloLunar.altura * 0.5);
+    //Determina o tamanho da chama
+    contexto.lineTo(0, moduloLunar.altura * 0.5 + Math.random() * 35 );
+    contexto.closePath();
+    contexto.fillStyle = "orange";
+    contexto.fill();
+}
+
+function mostrarVelocidade(){
+    contexto.font = "bold 18px Arial";
+    contexto.textAlign = "center";
+    contexto.textBaseLine = "middle";
+    contexto.fillStyle = "lightgray";
+    let velocidade = `Velocidade: ${(10 * moduloLunar.velocidade.y).toFixed(2)}`;
+    contexto.fillText(velocidade, 100, 60);
+}
+
+function mostrarCombustivel(){
+    contexto.font = "bold 18px Arial";
+    contexto.textAlign = "center";
+    contexto.textBaseLine = "middle";
+    contexto.fillStyle = "lightgray";
+    let combustivel = `Combustível: ${(moduloLunar.combustivel).toFixed(0)}`;
+    contexto.fillText(combustivel, 100, 80);
+}
+
+
+function desenhar(){
+    //limpar a tela
+    contexto.clearRect(0, 0, canvas.width, canvas.height);
+    //Esta função atualiza a posição do módulo lunar em função da gravidade
+    
+    atracaoGravitacional();
+    desenharModuloLunar();
+    mostrarVelocidade();
+    mostrarCombustivel();
+    //Esta função repete a execução da função desenhar a cada quadro
+    if(moduloLunar.posicao.y >= (canvas.height - 0.5 * moduloLunar.altura )){
+        
+        if(moduloLunar.velocidade.y >= 0.5){
+            return alert("Você morreu na queda!");
+        }else{
+            return alert("Você conseguiu pousar!")
+        }
+    }
+    requestAnimationFrame(desenhar);
+}
+
+
+//Seção de controle
+
+
+//Pressionando a seta para cima para ligar o motor
+document.addEventListener("keydown", teclaPressionada);
+function teclaPressionada(evento){
+    if(evento.keyCode == 38 && moduloLunar.combustivel > 0){
+        moduloLunar.motorLigado = true;
+        
+    }
+}
+//Soltando a seta para cima para desligar o motor
+document.addEventListener("keyup", teclaSolta);
+function teclaSolta(evento){
+    if(evento.keyCode == 38){
+        moduloLunar.motorLigado = false;
+    }
+}
+
+function consumoCombustivel(){
+    if(moduloLunar.combustivel > 0){
+        moduloLunar.combustivel--;
+    }else{
+        moduloLunar.combustivel = 0;
+        moduloLunar.motorLigado = false;
+    }
+}
+
+let gravidade = 0.01;
+function atracaoGravitacional(){
+    moduloLunar.posicao.x += moduloLunar.velocidade.x;
+    moduloLunar.posicao.y += moduloLunar.velocidade.y;
+    if(moduloLunar.motorLigado){
+        moduloLunar.velocidade.y -= 0.0115;
+    }
+    moduloLunar.velocidade.y += gravidade;
+   
+    
+}
+desenhar();
